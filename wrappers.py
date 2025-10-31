c9db425beee813479cff522badabb3fb3ad1f442
njambiEvelyne <evelynenganga76@gmail.com>
2025-10-31 21:42:47 +0300

    Added a rectification in the code

---
+++ wrappers.py
@@ -0,0 +1,15 @@
+#Wrappers are functions that enhance/modifies another function without changing its original code
+def validate_positive(func):
+    def wrapper(*args, **kwargs):
+        for arg in args:
+            if isinstance(arg, (int, float)) and arg < 0:
+                raise ValueError("All arguments must be positive")
+        return func(*args, **kwargs)
+    return wrapper
+
+@validate_positive
+def calculate_area(length, width):
+    return length * width
+
+print(calculate_area(5, 3))  # Works: 15
+#print(calculate_area(-5, 3)) # Raises ValueError
